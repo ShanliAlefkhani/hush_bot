071b430b3ccb173be6aa6f509ad64f215766b335
Shanli <shanli.alefkhani@tapsi.net>
2024-01-19 14:16:54 +0330

    Add start command handler

---
+++ hush_bot.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"Hello! You can now send me another user's ID and I will HUSH them.")
+
+def main() -> None:
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
+
