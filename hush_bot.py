f76f9c95cda328be18f6df81f55e9d0ee60f43fa
Shanli <shanli.alefkhani@tapsi.net>
2024-01-20 21:04:34 +0330

    Add hush_user

--- hush_bot.py
+++ hush_bot.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import filters, ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes
 
 load_dotenv()
 
@@ -41,6 +41,30 @@ def save_user_info(username, chat_id):
             connection.close()
 
 
+def get_user_id_from_username(username):
+    try:
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+
+        cursor = connection.cursor()
+
+        username = username.lower()
+
+        cursor.execute("SELECT chat_id FROM user_info WHERE username = %s", (username,))
+        result = cursor.fetchone()
+
+        return result[0] if result else None
+
+    finally:
+        if connection:
+            connection.close()
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
     username = update.message.from_user.username
@@ -48,9 +72,22 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     await update.message.reply_text(f"Hello! You can now send me another user's ID and I will HUSH them.")
 
+
+async def hush_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    target_username = update.message.text
+    if target_username[0] == '@':
+        target_username = target_username[1:]
+    target_user_id = get_user_id_from_username(target_username)
+    message_text = "HUSH"
+
+    await context.bot.send_message(chat_id=target_user_id, text=message_text)
+    await update.message.reply_text(f"Message sent to user with ID {target_username}.")
+
+
 if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), hush_user))
 
     app.run_polling()
