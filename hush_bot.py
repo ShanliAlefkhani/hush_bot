3d71b9001a4d25f5367b5d224230c98978c06e1b
Shanli <shanli.alefkhani@tapsi.net>
2024-01-21 08:37:14 +0330

    Add feedback ConversationHandler

--- hush_bot.py
+++ hush_bot.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 from telegram import Update
-from telegram.ext import filters, ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes
+from telegram.ext import filters, ApplicationBuilder, CommandHandler, ConversationHandler, MessageHandler, ContextTypes
 
 load_dotenv()
 
@@ -12,6 +12,8 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+FEEDBACK_MESSAGE = range(1)
 
 
 def save_user_info(username, chat_id):
@@ -33,6 +35,31 @@ def save_user_info(username, chat_id):
             "INSERT INTO user_info (username, chat_id) VALUES (%s, %s) ON CONFLICT (username) DO UPDATE SET chat_id = "
             "EXCLUDED.chat_id",
             (username, chat_id)
+        )
+
+        connection.commit()
+
+    finally:
+        if connection:
+            connection.close()
+
+
+def save_feedback(username, message_text):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+
+        cursor = connection.cursor()
+
+        cursor.execute(
+            "INSERT INTO feedback (username, message_text) VALUES (%s, %s)",
+            (username, message_text)
         )
 
         connection.commit()
@@ -75,6 +102,25 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"Hello! You can now send me another user's ID and I will HUSH them.")
 
 
+async def feedback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Please send your feedback.")
+    return FEEDBACK_MESSAGE
+
+
+async def feedback_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    username = update.message.from_user.username
+    message_text = update.message.text
+    save_feedback(username, message_text)
+
+    await update.message.reply_text("Thank you for your feedback.")
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bye! I hope we can talk again some day.")
+    return ConversationHandler.END
+
+
 async def hush_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     target_username = update.message.text
     if target_username[0] == '@':
@@ -91,6 +137,13 @@ if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('feedback', feedback)],
+        fallbacks=[CommandHandler("cancel", cancel)],
+        states={
+            FEEDBACK_MESSAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, feedback_message)],
+        },
+    ))
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), hush_user))
 
     app.run_polling()
